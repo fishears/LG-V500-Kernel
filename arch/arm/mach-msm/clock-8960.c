6ac0f3db9a02f722334a548ea522e494012e5d64
fishears <f1she4rs@gmail.com>
2015-10-22 11:47:45 +0100

    GPU Overclock: correct VDD_DIG_NOMINAL (reverted from commit 9470a71cd00ccabf030f1d93fcbb6de628c2ff13)

--- arch/arm/mach-msm/clock-8960.c
+++ arch/arm/mach-msm/clock-8960.c
@@ -3590,7 +3590,7 @@ static unsigned long fmax_gfx3d_8064ab[VDD_DIG_NUM] = {
 
 static unsigned long fmax_gfx3d_8064[VDD_DIG_NUM] = {
 	[VDD_DIG_LOW]     = 128000000,
-	[VDD_DIG_NOMINAL] = 320000000,
+	[VDD_DIG_NOMINAL] = 325000000,
 #ifdef CONFIG_GPU_OVERCLOCK
 	[VDD_DIG_HIGH]    = 487500000
 #else
