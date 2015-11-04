8e061e75583d959e6690612239b42a31aa0882b8
fishears <f1she4rs@gmail.com>
2015-11-04 16:44:11 +0000

    cpufreq: better fix for linaro 4.9

--- drivers/cpufreq/cpufreq_intelliactive.c
+++ drivers/cpufreq/cpufreq_intelliactive.c
@@ -864,8 +864,8 @@ static ssize_t show_target_loads(
 	for (i = 0; i < ntarget_loads; i++)
 		ret += sprintf(buf + ret, "%u%s", target_loads[i],
 			       i & 0x1 ? ":" : " ");
-
-	sprintf(buf + ret - 1, "\n");
+	--ret;
+	ret += sprintf(buf + ret, "\n");
 	spin_unlock_irqrestore(&target_loads_lock, flags);
 	return ret;
 }
@@ -907,8 +907,8 @@ static ssize_t show_above_hispeed_delay(
 	for (i = 0; i < nabove_hispeed_delay; i++)
 		ret += sprintf(buf + ret, "%u%s", above_hispeed_delay[i],
 			       i & 0x1 ? ":" : " ");
-
-	sprintf(buf + ret - 1, "\n");
+	--ret;
+	ret += sprintf(buf + ret, "\n");
 	spin_unlock_irqrestore(&above_hispeed_delay_lock, flags);
 	return ret;
 }
