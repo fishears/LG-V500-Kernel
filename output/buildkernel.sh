7a25b6fc340d6b858fa061e6ccc58da0f9ddea06
fishears <f1she4rs@gmail.com>
2016-07-10 10:44:58 +0100

    fs: switch from Tuxera exFAT to exfat-nofuse
    see https://github.com/dorimanx/exfat-nofuse
    
    (cherry picked from commit 53c274bbec7bb29f6b3e0565f2c8e6297bd6ff1e)
    Signed-off-by: fishears <f1she4rs@gmail.com>

--- output/buildkernel.sh
+++ output/buildkernel.sh
@@ -1,10 +1,12 @@
+
 #!/bin/bash
-BASE_FISH_VER="REDUX_v500"
+BASE_FISH_VER="REDUX"
 VER=""
 FISH_VER=$BASE_FISH_VER$VER
+ZIP_VER=$BASE_FISH_VER"_v500_"$VER
 
 export LOCALVERSION="-"`echo $FISH_VER`
-export CROSS_COMPILE=./arm-eabi-4.8/bin/arm-eabi-
+export CROSS_COMPILE=./linaro/bin/arm-eabi-
 export ARCH=arm
 export SUBARCH=arm
 export KBUILD_BUILD_USER=fishears
@@ -20,16 +22,18 @@ echo "ARCH="$ARCH
 echo "MODULES_DIR="$MODULES_DIR
 echo "KERNEL_DIR="$KERNEL_DIR
 echo "OUTPUT_DIR="$OUTPUT_DIR
-
+#build kernel
 cd $KERNEL_DIR
 make awifi-perf_defconfig
-make -j4
-
+make -j8
+#make boot.img, bump, copy modules, and zip kernel
 cd $OUTPUT_DIR
-rm zImage zipfile/boot.img bootin.img
 cp $KERNEL_DIR'/arch/arm/boot/zImage' .
 mkbootimg --base 0 --pagesize 2048 --kernel_offset 0x80208000 --ramdisk_offset 0x82200000 --second_offset 0x81100000 --tags_offset 0x80200100 --cmdline 'console=ttyHSL0,115200,n8 user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.hardware=awifi vmalloc=600M androidboot.bootdevice=msm_sdcc.1  androidboot.selinux=permissive' --kernel zImage --ramdisk ramdisk.cpio.gz -o bootin.img
 ./bump.py bootin.img zipfile/boot.img
 find $KERNEL_DIR -name "*.ko" -exec cp {} zipfile/system/lib/modules/ \;
 cd zipfile
-zip -r9 ../$FISH_VER'.zip' *
+zip -r9 ../$ZIP_VER'.zip' *
+#cleanup
+cd ..
+rm zImage zipfile/boot.img bootin.img zipfile/system/lib/modules/*.ko
